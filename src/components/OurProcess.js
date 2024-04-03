6b113475313340f41e6b1c6a76cc469ab87c21d4
GeForceGroot <tiwarishreyansh007@gmail.com>
2024-04-04 02:17:26 +0530

    c-26

--- src/components/OurProcess.js
+++ src/components/OurProcess.js
@@ -6,49 +6,49 @@ const OurProcess = () => {
             <div className='container' id='orP'>
                 <div id='headorP'>
                     <h2 style={{ color: "#079070" }}>OUR PROCESS</h2>
-                    <h3>How We Make Impeccable Academic Essays</h3>
+                    <h2>How We Make Impeccable Academic Essays</h2>
                     <p style={{fontSize:'20px', textAlign:'center', marginTop:'15px', marginBottom:'15px'}}>We have served thousands of clients from a wide range of business domains from around the world. No matter what your core business is, our content writing team has got the finest content writing services that you and your audience would love.</p>
                 </div>
                 <div className='row'>
                     <div className="col ourCol">
-                        <h1 id='headOurP'>#1</h1>
+                        <h2 id='headOurP'>#1</h2>
                         <h2>Identify</h2>
                         <p id='paraOurp'>This is where the first contact happens when we identify the requirements of the students and what they need.</p>
                     </div>
                     <div className="col ourCol">
-                        <h1 id='headOurP'>#2</h1>
+                        <h2 id='headOurP'>#2</h2>
                         <h2>Identify</h2>
                         <p id='paraOurp'>This is where the first contact happens when we identify the requirements of the students and what they need.</p>
                     </div>
                     <div className="col ourCol">
-                        <h1 id='headOurP'>#3</h1>
+                        <h2 id='headOurP'>#3</h2>
                         <h2>Identify</h2>
                         <p id='paraOurp'>This is where the first contact happens when we identify the requirements of the students and what they need.</p>
                     </div>
                     <div className="col ourCol">
-                        <h1 id='headOurP'>#4</h1>
+                        <h2 id='headOurP'>#4</h2>
                         <h2>Identify</h2>
                         <p id='paraOurp'>This is where the first contact happens when we identify the requirements of the students and what they need.</p>
                     </div>
                 </div>
                 <div className='row'>
                     <div className="col ourCol1">
-                        <h1 id='headOurP'>#5</h1>
+                        <h2 id='headOurP'>#5</h2>
                         <h2>Identify</h2>
                         <p id='paraOurp'>This is where the first contact happens when we identify the requirements of the students and what they need.</p>
                     </div>
                     <div className="col ourCol1">
-                        <h1 id='headOurP'>#6</h1>
+                        <h2 id='headOurP'>#6</h2>
                         <h2>Identify</h2>
                         <p id='paraOurp'>This is where the first contact happens when we identify the requirements of the students and what they need.</p>
                     </div>
                     <div className="col ourCol1">
-                        <h1 id='headOurP'>#7</h1>
+                        <h2 id='headOurP'>#7</h2>
                         <h2>Identify</h2>
                         <p id='paraOurp'>This is where the first contact happens when we identify the requirements of the students and what they need.</p>
                     </div>
                     <div className="col ourCol1">
-                        <h1 id='headOurP'>#8</h1>
+                        <h2 id='headOurP'>#8</h2>
                         <h2>Identify</h2>
                         <p id='paraOurp'>This is where the first contact happens when we identify the requirements of the students and what they need.</p>
                     </div>
