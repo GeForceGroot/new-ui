47782cf55d64ab0c57011248deadca379f542626
GeForceGroot <tiwarishreyansh007@gmail.com>
2024-04-03 08:36:13 +0530

    c-5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Carousel from './components/Carousel.JS';
-import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    {/* <Carousel/> */}
     </>
   );
 }
