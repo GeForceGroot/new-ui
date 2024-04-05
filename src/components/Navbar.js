e5642a14d5fb2ad00c2865091da71b9d3cfeeac4
GeForceGroot <tiwarishreyansh007@gmail.com>
2024-04-06 01:39:47 +0530

    commit-35

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,7 +7,7 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg navbar-light" style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)', backgroundColor: "white", border: "2px solid lightgray", position: "sticky", top: '0', zIndex: '1', opacity: '.8', height:'92px' }}>
                 <div className='container'>
                     <a className="navbar-brand" href="/">
-                        <img style={{ borderRadius: '25px' }} src="https://media.istockphoto.com/id/1197901679/vector/open-book-color-vector-template-icon.jpg?s=612x612&w=0&k=20&c=XdFcmrOHtGYGh6ab4YplsEpPzaLS4WUeeO6Nr6aaols=" width="40" height="40" alt="logo" />
+                        <img style={{ borderRadius: '25px' }} src="https://thumbs.dreamstime.com/b/tick-icon-assignment-paper-e-learning-education-concept-study-program-line-icon-logo-tick-icon-assignment-paper-e-learning-287631055.jpg" width="40" height="40" alt="logo" />
                     </a>
                     <a className="navbar-brand" href="/" style={{ fontSize: "20px" }}>Navbar</a>
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
