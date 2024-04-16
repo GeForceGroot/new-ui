2d5c5bd84bff98fce4dd2d300a81c825e45f1f3e
GeForceGroot <tiwarishreyansh007@gmail.com>
2024-04-17 01:20:18 +0530

    commit-71

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -8,10 +8,10 @@ const Popup = ({ handleClose, show }) => {
 
     return (
         <div className={showHideClassName} >
-            <section className="popup-main" style={{ textAlign: 'center', borderRadius: '10px', backgroundColor: "lightblue" }}>
-                <div className='d-flex'>
-                    <h6>Quick response and express delivery guaranteed !!!</h6>
-                    <button onClick={handleClose} style={{ backgroundColor:'lightblue'}}>
+            <section className="popup-main" style={{ textAlign: 'center', justifyContent:'center',borderRadius: '10px', backgroundColor: "lightblue" }}>
+                <div className='d-flex' style={{textAlign:'center'}}>
+                    <h6 style={{textAlign:"center"}}>Quick response and express delivery guaranteed !!!</h6>
+                    <button onClick={handleClose} style={{ position:'fixed',right:'0', top:'0',backgroundColor: 'lightblue' }}>
                         <img src="https://icons.veryicon.com/png/o/miscellaneous/medium-thin-linear-icon/cross-23.png" width={"20px"} height={"20px"} alt="" />
                     </button>
                 </div>
